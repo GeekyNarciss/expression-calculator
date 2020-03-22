532c03779815975fd3e8cbe370e66aa88a828d2f
Artemiy <GeekyNarciss@users.noreply.github.com>
2020-03-22 22:19:04 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -2,69 +2,86 @@ function eval() {
     // Do not use eval!!!
     return;
 }
+  
+function expressionCalculator(expr) { 
+    // https://www.youtube.com/watch?v=Vk-tGND2bfc  // solution algorithm
+    const expArray = expr.match(/\d+|\+|\-|\*|\/|\(|\)/g); // spaces deletion
+            leftBrackets = expr.replace(/[^(]/g, ''),
+            rightBrackets = expr.replace(/[^)]/g, ''); 
 
-function expressionCalculator(expr) {
-    let numberArr = [],
-        operationArr = [];
-
-    const operations = {
-        '+': {
-            do: (x, y) => x + y,
-            priority: 1
-        },
-        '-': {
-            do: (x, y) => x - y,
-            priority: 1
-        },
-        '*': {
-            do: (x, y) => x * y,
-            priorty: 2
-        },
-        '/': {
-            do: (x, y) => x / y,
-            priority: 2
-        },
-    };
-
-    function calculate(a, b, operation) {
-        if (operation === '/' && a === 0) {
-          throw new Error('TypeError: Division by zero.');
-        } // division by zero
-        return operations[operation].do(b, a);
-    }
-     
-    expr = expr.replace(/\s/g, ""); // spaces deletion
-
-    let openBracket = 0;
-    let closeBracket = 0;
-    for (let i = 0; i < expr.length; i++) {
-        if (expr[i] === '(') {
-            openBracket++;
+        if(leftBrackets.length != rightBrackets.length) {
+        throw new Error('ExpressionError: Brackets must be paired');//brackets error
         }
-        else if (expr[i] === ')') {
-            closeBracket++;
-        }
-    }
-    if (openBracket != closeBracket) {
-        throw "ExpressionError: Brackets must be paired";
-    } // brackets error
+        let numberArr = [],
+            operatorArr = [];
+        const operators = {
+            '+': {
+            priority: 1,
+            do: (a, b) => a + b
+            },
+            '-': {
+            priority: 1,
+            do: (a, b) => a - b
+            },
+            '/': {
+            priority: 2,
+            do: (a, b) => a / b
+            },
+            '*': {
+            priority: 2,
+            do: (a, b) => a * b
+            },
+            '(': {},
+            ')': {}
+        };
 
-    for (let i = 0; i < expr.length; i++) {
-        if (!isNaN(expr[i])) {
-          numberArr.push(Number(expr[i]));
+        function calculate(a, b, operator) {
+            if (operator === '/' && a === 0) {
+            throw new Error('TypeError: Division by zero.');
+            }
+            return operators[operator].do(b, a); //devision by 0
         }
-    }
+
+        //push numbers into number stack...
+        for (let i = 0; i < expArray.length; i++) {
+            if (!isNaN(expArray[i])) {
+            numberArr.push(Number(expArray[i]));
+            }//push numbers into stack
+
+            function bracketCount() {  //number of brackets
+                if (operatorArr[operatorArr.length - 1] == '(') {
+                return;
+                }
+                numberArr.push(calculate(numberArr.pop(), numberArr.pop(), operatorArr.pop()));  //calculate whats on top
+                bracketCount();
+            }
+            
+            function addOperator() {
+            if (!operatorArr.length || operatorArr[operatorArr.length - 1] == '(' || expArray[i] == '(' ) {
+                operatorArr.push(expArray[i]); // push operator into stack
+            } else if (expArray[i] === ')') {
+                bracketCount();
+                operatorArr.pop();
+            } else if (operators[expArray[i]].priority > operators[operatorArr[operatorArr.length - 1]].priority) {
+                operatorArr.push(expArray[i]);
+            } else {
+                numberArr.push(calculate(numberArr.pop(), numberArr.pop(), operatorArr.pop()));
+                addOperator();
+            }
+            }
+
+            if (expArray[i] in operators) {addOperator();}
+        }
+        
+        function stopCounting() {
+            if (!operatorArr.length) { return;}
+            numberArr.push(calculate(numberArr.pop(), numberArr.pop(), operatorArr.pop()));
+            stopCounting();
+        }
+        stopCounting();
+        return numberArr[0];
+        }
     
-    function bracketCount() {  //count number of brackets
-        if (operationArr[operationArr.length - 1] == '(') {
-          return;
-        }
-        numberArr.push(calculate(numberArr.pop(), numberArr.pop(), operationArr.pop()));  // calculate whats on top
-        bracketCount();
-      }
-
-}
-
-module.exports = {
-    expressionCalculator
-}
+    module.exports = {
+      expressionCalculator
+    };
